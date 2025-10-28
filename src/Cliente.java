b64984e0e683cda40741ace225b36b56a50e063e
SpookyyMoon <spookyluna666@gmail.com>
2025-10-28 18:06:05 +0100

    Commit inicial

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,19 +1,39 @@
-package com.example.joincafeteria;
+// Documentación empleada para el uso de notify, wait y synchronized: https://www.geeksforgeeks.org/java/java-notify-method-in-threads-synchronization-with-examples/
 
-public class Cliente extends Thread{
+import java.util.List;
 
+public class Cliente extends Thread {
     String nombreCliente;
     int tiempoEspera;
-    boolean atendidoCliente;
+    boolean atendido = false;
+    List<Cliente> listaClientes;
 
-    public Cliente(String nombreCliente, int tiempoEspera, boolean atendidoCliente) {
+    public Cliente(String nombreCliente, int tiempoEspera, List<Cliente> listaClientes) {
         this.nombreCliente = nombreCliente;
         this.tiempoEspera = tiempoEspera;
-        this.atendidoCliente = atendidoCliente;
+        this.listaClientes = listaClientes;
     }
 
     @Override
     public void run() {
-        super.run();
+        System.out.println("El cliente " + nombreCliente + " ha llegado a la cafetería!");
+
+        synchronized (listaClientes) { // Sincroniza la lista entre todos los hilos
+            listaClientes.add(this); // Se coloca al final de la cola
+            listaClientes.notifyAll(); // Notifica a los camareros en espera
+        }
+
+        try {
+            Thread.sleep(tiempoEspera);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (!atendido) {
+            System.out.println("El cliente " + nombreCliente + " se ha marchado sin ser atendido, dejará una mala reseña!");
+            synchronized (listaClientes) { // Elimina al cliente que se ha marchado de la lista
+                listaClientes.remove(this);
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        // Lista de clientes
+        ArrayList<Cliente> listaClientes = new ArrayList<>();
+
+        // Lista camareros
+        ArrayList<Camarero> listaCamareros = new ArrayList<>();
+
+        // Clientes
+        Cliente clienteUno = new Cliente("John Doe", 9000, listaClientes);
+        Cliente clienteDos = new Cliente("Jane Doe", 15000, listaClientes);
+        Cliente clienteTres = new Cliente("Pepe Pérez", 12000, listaClientes);
+        Cliente clienteCuatro = new Cliente("Pepa Pérez", 13000, listaClientes);
+        Cliente clienteCinco = new Cliente("Luis Garcia", 14000, listaClientes);
+        Cliente clienteSeis = new Cliente("María Lopez", 11000, listaClientes);
+        Cliente clienteSiete = new Cliente("Carlos Ruiz", 10000, listaClientes);
+        Cliente clienteOcho = new Cliente("Ana Gomez", 16000, listaClientes);
+
+        // Camareros
+        Camarero camareroUno = new Camarero("Camarero Uno", listaClientes);
+        Camarero camareroDos = new Camarero("Camarero Dos", listaClientes);
+
+        listaCamareros.add(camareroUno);
+        listaCamareros.add(camareroDos);
+
+        camareroUno.start();
+        camareroDos.start();
+
+        // Inicio de clientes
+        Cliente[] clientes = {clienteUno, clienteDos, clienteTres, clienteCuatro, clienteCinco, clienteSeis, clienteSiete, clienteOcho};
+        for (Cliente c : clientes) {
+            c.start();
+            try{ // Los clientes tardan entre 1 y 3 segundos en ir llegando desde el primero
+                int retrasoClientes = (int) (Math.random() * 3000) + 1000;
+                Thread.sleep(retrasoClientes);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        // Join de clientes
+        for (Cliente c : clientes) {
+            c.join();
+        }
+
+        // Interrumpe a los camareros para salir del bucle
+        for (Camarero camarero : listaCamareros) {
+            camarero.interrupt();
+        }
+
+        for (Camarero camarero : listaCamareros) {
+            camarero.join();
         }
     }
 }
